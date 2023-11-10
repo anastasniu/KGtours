c838674f84e674eab486f73562dad3d298e3bd12
anastasniu <nigit uanastasiia@gmail.com>
2023-11-10 23:59:02 +0600

    accounts serializer, views, urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from .serializers import SignUpSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import SignUpSerializer, UsersListSerializer, LogOutSerializer
+from .models import User
 
-# Create your views here.
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UsersListSerializer
+
+
+
+class SignUpView(generics.GenericAPIView):
+    serializer_class = SignUpSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': SignUpSerializer(user).data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+    
+class LogOutView(generics.GenericAPIView):
+    serializer_class = LogOutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
