c8fe1e59325424277e20fb06e29e2ddec95e04da
11MIKE1 <dorosukmihail1@gmail.com>
2023-11-11 00:30:29 +0600

    finish userprofile

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, Gallery
 
-# Register your models here.
+
+
+class GalleryInline(admin.TabularInline):
+    fk_name = 'profile'
+    model = Gallery
+
+
+@admin.register(Profile)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [GalleryInline,]
